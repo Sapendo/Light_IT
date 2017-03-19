a3c7061997227feb93dbc071fdae83426276914b
Вячеслав Горбань < >
2017-03-19 23:11:46 +0200

    some change

--- JavaScript/JS.js
+++ JavaScript/JS.js
@@ -14,29 +14,21 @@
           console.log(items);
           var countMale = 0;
           var countFemail = 0;
+          var shortInfo;
+          var moreInfo;
+          var allInfo;
           for (var i = 0, j = 1; i < items.results.length; i++, j++) {
               item = items.results[i];
-
               if (item.gender === 'male') {
                   countMale++;
               } else countFemail++;
-              var shortInfo = '<tr class="shortInfo">';
-              shortInfo += '<td><img src="' + item.picture.thumbnail + '" alt="photoPerson"></td>' + '<td>' + item.name.last + '</td><td>' + item.name.first + '</td><td>' + item.login.username + '</td><td>' + item.phone + '</td><td>' + item.location.city + '</td><td><button class="buttonInfo"><i class="fa fa-plus" aria-hidden="true"></i></button></td>';
-
-              shortInfo += '</tr>';
-
-              var moreInfo = '<tr class="moreInfo">';
-
-              moreInfo +=  '<td>' + item.name.first + '</td><td>' + item.name.last + '</td><td>' + item.login.username + '</td><td>' + item.registered + '</td><td>' + item.email + '</td><td>' + item.dob + '</td><td>' + item.phone + '</td><td>' + item.cell + '</td><td><img src="' + item.picture.large + '" alt="photoPerson"></td>';
-
-              moreInfo += '</tr>';
-
-              var allInfo = shortInfo + moreInfo;
-
+             
+              shortInfo = '<tr class="shortInfo"><td><img src="' + item.picture.thumbnail + '" alt="photoPerson"></td>' + '<td>' + item.name.last + '</td><td>' + item.name.first + '</td><td>' + item.login.username + '</td><td>' + item.phone + '</td><td>' + item.location.city + '</td><td><button class="buttonInfo"><i class="fa fa-plus" aria-hidden="true"></i></button></td></tr>';  
+              
+              moreInfo =  '<tr class="moreInfo"><td>' + item.name.first + '</td><td>' + item.name.last + '</td><td>' + item.login.username + '</td><td>' + item.registered + '</td><td>' + item.email + '</td><td>' + item.dob + '</td><td>' + item.phone + '</td><td>' + item.cell + '</td><td><img src="' + item.picture.large + '" alt="photoPerson"></td></tr>';
+              
+              allInfo = shortInfo + moreInfo;
               $('#tbody').append(allInfo);
-
-
-
           }
 
           (function($) {
@@ -46,24 +38,19 @@
                   $(this).parent().parent().next().slideDown();
                   return false;
               });
-
           })(jQuery);
-
-
+          
           $(".shortInfo:odd").css("background-color", "#CCCCCC");
           $("#tbody tr>td").addClass("text-center");
+          $(".shortInfo td").css("vertical-align","middle");
           $("img").css({
               "border-radius": "50%",
               "border-style": "solid",
               "border-width": "3px",
               "border-color": "#fff"
           });
-
-
-
           var tagChart = 'chart';
           counterChart(countMale, countFemail, tagChart);
-
       }
   }
   request.send();
@@ -73,10 +60,6 @@
       $("#showStatistic").click(windowShow);
       $("#hideStatistic").click(windowHideSlowly);
   });
-
-
-
-
 
   function windowShow() {
       $("#chartArea").show(1000);
@@ -93,20 +76,18 @@
   function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
   } //Random number
+
   function counterChart(m, f, t) {
       google.charts.load('current', {
           'packages': ['corechart']
       });
       google.charts.setOnLoadCallback(drawChart);
-
       function drawChart() {
-
           var data = google.visualization.arrayToDataTable([
               ['Gender', 'Count'],
               ['Male', m],
               ['Female', f]
           ]);
-
           var options = {
               title: 'Gender Activities',
               width: '700',
@@ -118,17 +99,16 @@
               backgroundColor: '#eee',
               pieSliceText: 'none'
           };
-
           var chart = new google.visualization.PieChart(document.getElementById(t));
-
           return chart.draw(data, options);
       }
   } //Create graph
+
   function mySearch() {
       var input, filter, table, tr, td;
       input = document.getElementById("search");
       filter = input.value.toUpperCase();
-      tr = document.getElementById("tbody").getElementsByTagName('tr');
+      tr = document.getElementById("tbody").getElementsByClassName('shortInfo');
       console.log(tr);
       for (var i = 0; i < tr.length; i++) {
           console.log(tr[i].getElementsByTagName("td")[2]);
